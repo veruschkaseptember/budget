899ac544b778cef74d9d7a48674005646ee09df8
Veruschka <veruschkaseptember@yahoo.com>
2022-01-18 21:07:25 +0200

    First commit - expense tracker

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+
+const Main = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Main
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Line 6, only time accessing the real DOM, App go inside of the document and target the root which an empty div inside HTML
