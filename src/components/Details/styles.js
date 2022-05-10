f9441d87902e160d3657b7f191a9993ce09f299e
Veruschka <info@veruschkanatasja.com>
2022-05-10 15:15:28 +0200

    final commit

--- src/components/Details/styles.js
+++ src/components/Details/styles.js
@@ -2,11 +2,11 @@ import  { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
         income: {
-            borderBottom: '15px solid rgba(2, 146, 84, 1.0)',
+            borderBottom: '8px solid rgba(2, 146, 84, 1.0)',
             
         },
         expense: {
-            borderBottom: '15px solid rgba(217, 66, 66, 1.0)',
+            borderBottom: '8px solid rgba(217, 66, 66, 1.0)',
         },
 }));
 
